478ca08c74e31a3197f0c679a440a795b794053a
DESKTOP-O3LMC10\User <aisw.snp@gmail.com>
2024-03-06 17:53:37 +0900

    2일차 수업내용

--- java-basic/src/kr/ac/kopo/day01/HelloMain.java
+++ java-basic/src/kr/ac/kopo/day01/HelloMain.java
@@ -1,7 +1,17 @@
 package kr.ac.kopo.day01;
 
+/**
+ * print(), printf() 차이점을 알아봅시다
+ * 
+ * @author 홍길동
+ * @version 0.1
+ */
 public class HelloMain {
 
+	/**
+	 * 실행하기 위한 메소드
+	 * @param args 입력값
+	 */
 	public static void main(String[] args) {
 		
 		// 3 + 4
